4e1cf5c196420d2eefb0090910b70135511ecc2b
Mrpaker <spectator_w@126.com>
2019-03-28 22:37:19 +0800

    factory pattern

---
+++ The-Factory-Pattern/index.js
@@ -0,0 +1,46 @@
+function Car(options) {
+  this.doors = options.doors || 4;
+ this.state = options.state || "brand new";
+ this.color = options.color || 'black';
+}
+
+function Truck(options) {
+  this.state = options.state || "used";
+ this.wheelSize = options.wheelSize || "large";
+ this.color = options.color || 'blue';
+}
+
+function VehicleFactory() {}
+
+VehicleFactory.prototype.vehicleClass = Car;
+
+VehicleFactory.prototype.createVehicle = function (options) {
+ switch(options.vehicleType) {
+   case "car":
+     this.vehicleClass = Car;
+     break;
+   case "truck":
+     this.vehicleClass = Truck;
+     break;
+ }
+}
+
+var carFactory = new VehicleFactory();
+var car = carFactory.createVehicle({
+ vehicleType: "car",
+ color: "yellow",
+ doors: 6 
+});
+
+var movingTruck = carFactory.createVehicle({
+ vehicleType: "truck",
+ state: "like new",
+ color: "red",
+ wheelSize: "small" 
+});
+
+console.log(car instanceof Car);
+console.log( movingTruck instanceof Truck );
+
+console.log(car);
+console.log(movingTruck);
